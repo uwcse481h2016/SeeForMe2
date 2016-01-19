d7e181b25d79fc235354b047f1d0331dc7473bd3
kyeungk <kyeungk@uw.edu>
2016-01-19 13:59:43 -0800

    added two blank activities with back buttons

--- SeeForMe/app/src/main/java/edu/uw/seeforme/seeforme/MainActivity.java
+++ SeeForMe/app/src/main/java/edu/uw/seeforme/seeforme/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.uw.seeforme.seeforme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCR(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ColorRecognition.class);
+        startActivity(intent);
+    }
+
+    public void openOR(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ObjectRecognition.class);
+        startActivity(intent);
     }
 }
